168fb68913bf46155444ae15a896aaad80a09ba5
RofLeR8 <mister.kabalin@bk.ru>
2026-07-23 19:53:00 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,158 +1,82 @@
-import { useState } from "react";
 import { getToken } from "firebase/messaging";
 import { messaging } from "./firebase";
 
 
-function App() {
-
-    const [token, setToken] = useState("");
-    const [status, setStatus] = useState("");
+function App(){
 
 
-    async function enableNotifications() {
-
-        try {
-
-            setStatus("Запрашиваю разрешение...");
+async function enableNotifications(){
 
 
-            const permission =
-                await Notification.requestPermission();
-
-
-            if (permission !== "granted") {
-
-                setStatus(
-                    "Разрешение отклонено"
-                );
-
-                return;
-            }
-
-
-            setStatus(
-                "Получаю FCM token..."
-            );
-
-
-            const currentToken =
-                await getToken(
-                    messaging,
-                    {
-                        vapidKey:
-                        "BLqt4dudqW4QntCjJsmerjgzfz4rV_2ZSDrAtea8XSh6qtw0ZXJW0sDA-pco2OqJfQxfANYBKkTtttzD58zbfgk"
-                    }
-                );
-
-
-            if (currentToken) {
-
-                console.log(
-                    currentToken
-                );
-
-
-                setToken(
-                    currentToken
-                );
-
-
-                setStatus(
-                    "Токен получен"
-                );
-
-
-            } else {
-
-                setStatus(
-                    "Firebase не вернул токен"
-                );
-
-            }
-
-
-        } catch(error) {
-
-            console.error(error);
-
-            setStatus(
-                error.message
-            );
-
-        }
-
-    }
+const permission =
+await Notification.requestPermission();
 
 
 
-    return (
+if(permission !== "granted"){
 
-        <div style={{
-            padding:"20px",
-            fontFamily:"Arial"
-        }}>
+alert(
+"Нет разрешения"
+);
 
+return;
 
-            <h1>
-                PWA Notification Test
-            </h1>
-
-
-            <button
-                onClick={enableNotifications}
-                style={{
-                    padding:"15px",
-                    fontSize:"18px"
-                }}
-            >
-                Включить уведомления
-            </button>
+}
 
 
 
-            <h3>
-                Статус:
-            </h3>
+const token =
+await getToken(
+messaging,
+{
 
-            <p>
-                {status}
-            </p>
+vapidKey:
+"BLqt4dudqW4QntCjJsmerjgzfz4rV_2ZSDrAtea8XSh6qtw0ZXJW0sDA-pco2OqJfQxfANYBKkTtttzD58zbfgk"
 
-
-
-            {
-                token &&
-
-                <div>
-
-                    <h3>
-                        FCM Token:
-                    </h3>
+}
+);
 
 
-                    <textarea
 
-                    value={token}
-
-                    readOnly
-
-                    style={{
-
-                        width:"100%",
-                        height:"200px"
-
-                    }}
-
-                    />
-
-                </div>
-
-            }
+console.log(
+"DEVICE TOKEN:",
+token
+);
 
 
-        </div>
 
-    );
+alert(
+"Токен получен. Смотри console"
+);
+
+
+
+}
+
+
+
+return (
+
+<div>
+
+<h1>
+PWA Notification Test
+</h1>
+
+
+<button
+onClick={enableNotifications}
+>
+
+Включить уведомления
+
+</button>
+
+
+</div>
+
+)
+
 
 }
 
